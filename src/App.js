671fab168269c20ff13457d20e2515ae0f0c3bc2
Mohammad Hasnain <mhasnain753@gmail.com>
2023-12-21 17:05:06 +0500

    Add Dev concepts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { UserList } from './components/UserList';
 import {DatePicker, Form} from 'antd';
 import moment from 'moment';
 import dayjs from 'dayjs';
+import UploadFile from './components/UploadFile';
+import FileUpload from './components/UploadFile';
+import { ShapeEditor } from './ReactDev/Array';
+import { CounterList } from './ReactDev/CounterList';
+import {Bucketlist, List, ReverseList} from './ReactDev/List'
 function App() {
   const [users,setUsers] = useState([]);
   const dispatchUserEvent = (actionType,payload)=>{
@@ -27,14 +32,24 @@ function App() {
         <AddUser/>
         <UserList/>
       </AppContext.Provider> */}
-      <Form initialValues={{start: dayjs().startOf('month'), end:moment().endOf('month')}}>
+      {/* <Form initialValues={{start: dayjs().startOf('month'), end:moment().endOf('month')}}>
         <Form.Item name='start' label='Start Date'>
           <DatePicker format={"YYYY-MM-DD"} placeholder='Start Date'/>
         </Form.Item>
         <Form.Item name='end' label='End Date'>
           <DatePicker format={"YYYY-MM-DD"} placeholder='End Date'/>
         </Form.Item>
-      </Form>
+      </Form> */}
+      {/* <UploadFile/> */}
+      {/* <div style={{ padding: 20 }}>
+        <h1>File Upload Example</h1>
+        <FileUpload />
+      </div> */}
+      {/* <ShapeEditor/> */}
+      {/* <CounterList/> */}
+      {/* <List/> */}
+      {/* <ReverseList/> */}
+      <Bucketlist/>
     </div>
   );
 }
